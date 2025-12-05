4d3f3d6d031e475c8d57b55651fc7f101b2bdb9d
Mario Culica <alpha9615@gmail.com>
2025-12-05 10:08:18 +0200

    Fixat bug la formular și adăugat stiluri noi

--- .history/little-lemon/src/Header_20251204170941.js
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <header>
-       <img src={require("./assets/lemon desserts")} alt="Little Lemon Logo" />
-    </header>
-  );
-}
-
-export default Header;
--- .history/little-lemon/src/Header_20251204171543.js
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <header>
-       <img src ={lemon.png} alt="Little Lemon Logo" />
-    </header>
-  );
-}
-
-export default Header;
